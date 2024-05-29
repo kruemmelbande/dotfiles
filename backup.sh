d6362a544fdca27f5b5390597c76d04c852bb5a8
kruemmelbande <kai.scheufen@gmail.com>
2024-05-29 12:29:40 +0200

    add krunner

--- backup.sh
+++ backup.sh
@@ -4,6 +4,8 @@ cp ~/.config/khotkeysrc .config/
 cp ~/.config/hypr .config/ -r
 cp ~/.config/konsolerc .config/
 cp ~/.config/yakuakerc .config/
+cp ~/.config/dolphinrc .config/
+cp ~/.config/krunnerrc .config/
 cp ~/.bashrc ./.bashrc
 cp /etc/locale.* etc/
 cp /etc/vconsole.conf etc/
