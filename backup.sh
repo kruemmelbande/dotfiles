c9f0ae5dbc046e7246ee0003b59bf20054807e2d
kruemmelbande <kai.scheufen@gmail.com>
2024-05-29 12:27:03 +0200

    add konsole and yakuake

--- backup.sh
+++ backup.sh
@@ -2,6 +2,9 @@ set -x
 cp ~/.config/linux-discord-rich-presencerc .config/
 cp ~/.config/khotkeysrc .config/
 cp ~/.config/hypr .config/ -r
+cp ~/.config/konsolerc .config/
+cp ~/.config/yakuakerc .config/
 cp ~/.bashrc ./.bashrc
 cp /etc/locale.* etc/
 cp /etc/vconsole.conf etc/
+git add .
